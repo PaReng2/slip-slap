2eeab9b910232e7b70c6c623d3e1e94a00b8ce3a
PaReng <korreppsj@naver.com>
2025-07-15 18:31:41 +0900

    [Feat] 플레이어 움직임 구현

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject player;
+    [Header("Game Objects")]
+    public GameObject verPlayer;
+    public GameObject horPlayer;
+
+    public GameObject verSlideBar;
+    public GameObject horSlideBar;
+    public GameObject enemyPrefab;
+
+    [Header("Player Stat")]
+    public int playerMoveSpeed = 5;
+
+    private bool isMoveHor;
+    private bool isMoveVer;
+
     void Start()
     {
-        
+        isMoveHor = true;
+        isMoveVer = true;
     }
-
 
     void Update()
     {
-        
+        Move();
     }
+
+    void Move()
+    {
+       if (Input.GetKey(KeyCode.A) && isMoveHor == true)
+        {
+            horPlayer.transform.Translate(-playerMoveSpeed * Time.deltaTime, 0, 0);
+        }
+
+       if (Input.GetKey(KeyCode.D) && isMoveHor == true)
+        {
+            horPlayer.transform.Translate(playerMoveSpeed * Time.deltaTime, 0, 0);
+        }
+
+      if (Input.GetKey(KeyCode.UpArrow) && isMoveVer == true)
+       {
+            verPlayer.transform.Translate(0, playerMoveSpeed * Time.deltaTime, 0);
+       }
+
+      if (Input.GetKey(KeyCode.DownArrow) && isMoveVer == true)
+        {
+            verPlayer.transform.Translate(0, -playerMoveSpeed * Time.deltaTime, 0);
+        }
+      
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.tag == "bounHor")
+    //    {
+    //        isMoveHor = false;
+    //    }
+    //    else
+    //    {
+    //        isMoveHor = true;
+    //    }
+
+    //    if (collision.tag == "bounVer")
+    //    {
+    //        isMoveVer = false;
+    //    }
+    //    else
+    //    {
+    //        isMoveVer = true;
+    //    }
+
+    //}
+
 }
