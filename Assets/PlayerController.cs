9b8501b13541b14e39429b2df58f8d1b9bad0a6d
PaReng <korreppsj@naver.com>
2025-07-16 06:10:41 +0900

    [Feat] 플레이어 이동 범위 제한

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Game Objects")]
-    public GameObject verPlayer;
-    public GameObject horPlayer;
+    [Header("Player Objects")]
+    public GameObject XObj;
+    public GameObject YObj;
+    public float PlayerMoveSpeed = 5f;
 
-    public GameObject verSlideBar;
-    public GameObject horSlideBar;
-    public GameObject enemyPrefab;
+    public float xMoveRange = 3f; // XObj가 움직일 수 있는 ±범위
+    public float yMoveRange = 3f; // YObj가 움직일 수 있는 ±범위
 
-    [Header("Player Stat")]
-    public int playerMoveSpeed = 5;
-
-    private bool isMoveHor;
-    private bool isMoveVer;
+    private Vector3 xObjStartPos;
+    private Vector3 yObjStartPos;
 
     void Start()
     {
-        isMoveHor = true;
-        isMoveVer = true;
+        // 각 오브젝트의 초기 위치 저장
+        xObjStartPos = XObj.transform.position;
+        yObjStartPos = YObj.transform.position;
     }
 
     void Update()
@@ -31,48 +29,34 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-       if (Input.GetKey(KeyCode.A) && isMoveHor == true)
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        // XObj 이동
+        if (moveX != 0)
         {
-            horPlayer.transform.Translate(-playerMoveSpeed * Time.deltaTime, 0, 0);
+            Vector3 newXPos = XObj.transform.position;
+            newXPos.x += moveX * PlayerMoveSpeed * Time.deltaTime;
+
+            // 기준 위치  범위로 Clamp
+            float minX = xObjStartPos.x - xMoveRange;
+            float maxX = xObjStartPos.x + xMoveRange;
+            newXPos.x = Mathf.Clamp(newXPos.x, minX, maxX);
+
+            XObj.transform.position = newXPos;
         }
 
-       if (Input.GetKey(KeyCode.D) && isMoveHor == true)
+        // YObj 이동
+        if (moveY != 0)
         {
-            horPlayer.transform.Translate(playerMoveSpeed * Time.deltaTime, 0, 0);
-        }
+            Vector3 newYPos = YObj.transform.position;
+            newYPos.y += moveY * PlayerMoveSpeed * Time.deltaTime;
 
-      if (Input.GetKey(KeyCode.UpArrow) && isMoveVer == true)
-       {
-            verPlayer.transform.Translate(0, playerMoveSpeed * Time.deltaTime, 0);
-       }
+            float minY = yObjStartPos.y - yMoveRange;
+            float maxY = yObjStartPos.y + yMoveRange;
+            newYPos.y = Mathf.Clamp(newYPos.y, minY, maxY);
 
-      if (Input.GetKey(KeyCode.DownArrow) && isMoveVer == true)
-        {
-            verPlayer.transform.Translate(0, -playerMoveSpeed * Time.deltaTime, 0);
+            YObj.transform.position = newYPos;
         }
-      
     }
-
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.tag == "bounHor")
-    //    {
-    //        isMoveHor = false;
-    //    }
-    //    else
-    //    {
-    //        isMoveHor = true;
-    //    }
-
-    //    if (collision.tag == "bounVer")
-    //    {
-    //        isMoveVer = false;
-    //    }
-    //    else
-    //    {
-    //        isMoveVer = true;
-    //    }
-
-    //}
-
 }
