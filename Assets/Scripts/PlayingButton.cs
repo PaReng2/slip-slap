22be1bdb3b61ffc0c80bd5b1713a2746a76b53d7
PaReng <korreppsj@naver.com>
2025-07-31 19:26:15 +0900

    [Feat] 버튼 씬 이동기능 추가

--- Assets/Scripts/PlayingButton.cs
+++ Assets/Scripts/PlayingButton.cs
@@ -1,12 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayingButton : MonoBehaviour
 {
-    public void OnClickRe()
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Title()
     {
         SceneManager.LoadScene(0);
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
